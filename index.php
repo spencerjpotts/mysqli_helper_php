c11f5098a4a8bb8ba0bcdcc40e60355da62177cc
Spencer <spencerjpotts@gmail.com>
2018-12-17 20:23:18 +1100

    Added html with a couple of tags and a item list of mysqli items selected using MySqlHelper::select().

--- index.php
+++ index.php
@@ -2,5 +2,28 @@
   # Include the class file mysqli_help.php from relative or linked file location/ directory.
   include 'mysqli_helper.php';
 
-  
+  $mysql_obj = new MySqlHelper('localhost', 'root', '', 'test');
 ?>
+<!DOCTYPE html>
+<html>
+  <head>
+    <title>MySqlHelper test html/php</title>
+  </head>
+  <body>
+
+    <h1>My First Heading</h1>
+    <p>My first paragraph.</p>
+
+    <ul>
+      <?php
+      $mysql_obj->setTable('person');
+      foreach($mysql_obj->select($args='name, id', $where='id = 1') as $value) {
+        echo "<li>";
+        echo $value['id'];
+        echo $value['name'];
+        echo "</li>";
+      }
+      ?>
+    </ul>
+  </body>
+</html>
